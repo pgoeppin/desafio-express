60d28a86ce49f25216ea00d1dc1f02689eab8f60
Pablo Goeppinger <pgoeppin@gmail.com>
2022-12-24 03:42:56 -0300

    fix: changing obj parameters, fixing errors with index.html

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ app.listen(3000, console.log("¡Servidor encendido! en el puerto 3000."));
 app.use(cors());
 app.use(express.json());
 
+// HTML 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+    })    
+
 // METODO POST - CREATE
 app.post("/canciones", async (req, res) => {
   try {
-    const songs = JSON.parse(await fs.promises.readFile("canciones.json"));
-    const song = req.body;
-    if (!song.id || !song.name || !song.artist || !song.tone) {
+    const canciones = JSON.parse(await fs.promises.readFile("canciones.json", "utf-8"));
+    const cancion = req.body;
+    if (!cancion.id || !cancion.titulo || !cancion.artista || !cancion.tono) {
       res.send(
-        "Falta un dato, por favor agregar una canción con los siguientes parametros: id, name, artist y tone"
+        "Falta un dato, por favor agregar una canción con los siguientes parametros: id, titulo, artista y tono"
       );
     } else {
-      if (!songs[songs.findIndex((p) => p.id == song.id)]) {
-        songs.push(song);
-        await fs.promises.writeFile("canciones.json", JSON.stringify(songs));
+      if (!canciones[canciones.findIndex((p) => p.id == cancion.id)]) {
+        canciones.push(cancion);
+        await fs.promises.writeFile("canciones.json", JSON.stringify(canciones));
         res.send("Cancion agregada con exito!");
       } else {
         res.send("Cancion ya existe, prueba con otro id");
@@ -33,9 +38,9 @@ app.post("/canciones", async (req, res) => {
 // METODO GET - READ
 app.get("/canciones", async (req, res) => {
   try {
-    const songs = JSON.parse(await fs.promises.readFile("canciones.json"));
-    if (songs.length != 0) {
-      res.json(songs);
+    const canciones = JSON.parse(await fs.promises.readFile("canciones.json", "utf-8"));
+    if (canciones.length != 0) {
+      res.json(canciones);
     } else {
       res.send("No hay canciones!");
     }
@@ -47,29 +52,21 @@ app.get("/canciones", async (req, res) => {
 // METODO PUT - UPDATE
 app.put("/canciones/:id", async (req, res) => {
   try {
-    const songs = JSON.parse(await fs.promises.readFile("canciones.json"));
+    const canciones = JSON.parse(await fs.promises.readFile("canciones.json", "utf-8"));
     const { id } = req.params;
-    const song = req.body;
-    const index = songs.findIndex((p) => p.id == id);
-    if (songs.length != 0) {
-      if (song.name) {
-        songs[index].name = song.name;
-        await fs.promises.writeFile("canciones.json", JSON.stringify(songs));
+    const cancion = req.body;
+    const index = canciones.findIndex((p) => p.id == id);
+    if (canciones.length != 0) {
+      if (cancion.titulo || cancion.artista || cancion.tono) {
+        canciones[index].titulo = cancion.titulo;
+        canciones[index].artista = cancion.artista;
+        canciones[index].tono = cancion.tono;
+        await fs.promises.writeFile("canciones.json", JSON.stringify(canciones));
         res.send(`Cancion con id ${id} modificada exitosamente!`);
       }
-      if (song.artist) {
-        songs[index].artist = song.artist;
-        await fs.promises.writeFile("canciones.json", JSON.stringify(songs));
-        res.send(`Cancion con id ${id} modificada exitosamente!`);
-      }
-      if (song.tone) {
-        songs[index].tone = song.tone;
-        await fs.promises.writeFile("canciones.json", JSON.stringify(songs));
-        res.send(`Cancion con id ${id} modificada exitosamente!`);
-      }
-      if (!song.name && !song.artist && !song.tone) {
+      if (!cancion.titulo && !cancion.artista && !cancion.tono) {
         res.send(
-          "Parametro a modificar no encontrado, cancion no modificada. Parametros: name, artist or tone"
+          "Parametro a modificar no encontrado, cancion no modificada. Parametros: titulo, artista o tono"
         );
       }
     } else {
@@ -84,12 +81,12 @@ app.put("/canciones/:id", async (req, res) => {
 app.delete("/canciones/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const songs = JSON.parse(await fs.promises.readFile("canciones.json"));
-    if (songs.length > 0) {
-      const index = songs.findIndex((p) => p.id == id);
+    const canciones = JSON.parse(await fs.promises.readFile("canciones.json", "utf-8"));
+    if (canciones.length > 0) {
+      const index = canciones.findIndex((p) => p.id == id);
       if (index != -1) {
-        songs.splice(index, 1);
-        await fs.promises.writeFile("canciones.json", JSON.stringify(songs));
+        canciones.splice(index, 1);
+        await fs.promises.writeFile("canciones.json", JSON.stringify(canciones));
         res.send(`Cancion eliminada con id: ${id}`);
       } else {
         res.send(`No existe la cancion con id ${id}`);
